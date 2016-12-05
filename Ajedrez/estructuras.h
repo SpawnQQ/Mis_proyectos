c710e68787d5fb8d8c3e82b273f47fd4c7e52e11
nicolasaguirre <nick.aguirre.16@gmail.com>
2016-12-05 00:41:20 -0300

    Creacion de estructuras e Inicio de tablero

---
+++ Ajedrez/estructuras.h
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include <stdio.h>
+#include<stdbool.h>
+
+struct tipo{
+	char nombre;
+	int posicion[2];
+};
+
+struct pieza{
+	char color;
+	struct tipo tipo_pieza;
+	bool primer_turno;
+	struct pieza *enlace;
+};
+	
