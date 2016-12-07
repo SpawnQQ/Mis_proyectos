71b4b27b0eb10d6a8be1c4f6c7fcfb09dc0cfca0
nicolasaguirre <nick.aguirre.16@gmail.com>
2016-12-07 16:10:02 -0300

    Se agrego una funcion set, para cambiar el valor de la matriz, se aplico al tablero de inicio

--- Ajedrez/metodos.h
+++ Ajedrez/metodos.h
@@ -8,94 +8,42 @@ void tablero_inicio(p (*m)[8]){
 	for(int i=0;i<8;i++){
 		for(int j=0;j<8;j++){
 			if(i==1){
-				m[i][j].color='b';
-				m[i][j].primer_turno=true;
-				m[i][j].tipo_pieza.nombre='P';
-				m[i][j].tipo_pieza.posicion[0]=i;
-				m[i][j].tipo_pieza.posicion[1]=j;
+				matriz_setPieza(m,i,j,'b',true,'P',i,j);
 			}else{
 				if(i==6){
-					m[i][j].color='w';
-					m[i][j].primer_turno=true;
-					m[i][j].tipo_pieza.nombre='P';
-					m[i][j].tipo_pieza.posicion[0]=i;
-					m[i][j].tipo_pieza.posicion[1]=j;	
+					matriz_setPieza(m,i,j,'w',true,'P',i,j);	
 				}else{
 					if(i==0 && (j==0 || j==7)){
-						m[i][j].color='b';
-						m[i][j].primer_turno=true;
-						m[i][j].tipo_pieza.nombre='R';
-						m[i][j].tipo_pieza.posicion[0]=i;
-						m[i][j].tipo_pieza.posicion[1]=j;					
+						matriz_setPieza(m,i,j,'b',true,'R',i,j);					
 					}else{
 						if(i==7 && (j==0 || j==7)){
-							m[i][j].color='w';
-							m[i][j].primer_turno=true;
-							m[i][j].tipo_pieza.nombre='R';
-							m[i][j].tipo_pieza.posicion[0]=i;
-							m[i][j].tipo_pieza.posicion[1]=j;
+							matriz_setPieza(m,i,j,'w',true,'R',i,j);
 						}else{
 							if(i==0 && (j==1 || j==6)){
-								m[i][j].color='b';
-								m[i][j].primer_turno=true;
-								m[i][j].tipo_pieza.nombre='H';
-								m[i][j].tipo_pieza.posicion[0]=i;
-								m[i][j].tipo_pieza.posicion[1]=j;
+								matriz_setPieza(m,i,j,'b',true,'H',i,j);
 							}else{
 								if(i==7 && (j==1 || j==6)){
-									m[i][j].color='w';
-									m[i][j].primer_turno=true;
-									m[i][j].tipo_pieza.nombre='H';
-									m[i][j].tipo_pieza.posicion[0]=i;
-									m[i][j].tipo_pieza.posicion[1]=j;
+									matriz_setPieza(m,i,j,'w',true,'H',i,j);
 								}else{
 									if(i==0 && (j==2 || j==5)){
-										m[i][j].color='b';
-										m[i][j].primer_turno=true;
-										m[i][j].tipo_pieza.nombre='B';
-										m[i][j].tipo_pieza.posicion[0]=i;
-										m[i][j].tipo_pieza.posicion[1]=j;
+										matriz_setPieza(m,i,j,'b',true,'B',i,j);
 									}else{
 										if(i==7 && (j==2 || j==5)){
-											m[i][j].color='w';
-											m[i][j].primer_turno=true;
-											m[i][j].tipo_pieza.nombre='B';
-											m[i][j].tipo_pieza.posicion[0]=i;
-											m[i][j].tipo_pieza.posicion[1]=j;
+											matriz_setPieza(m,i,j,'w',true,'B',i,j);
 										}else{
 											if(i==0 && j==3){
-												m[i][j].color='b';
-												m[i][j].primer_turno=true;
-												m[i][j].tipo_pieza.nombre='K';
-												m[i][j].tipo_pieza.posicion[0]=i;
-												m[i][j].tipo_pieza.posicion[1]=j;
+												matriz_setPieza(m,i,j,'b',true,'K',i,j);
 											}else{
 												if(i==0 && j==4){
-													m[i][j].color='b';
-													m[i][j].primer_turno=true;
-													m[i][j].tipo_pieza.nombre='Q';
-													m[i][j].tipo_pieza.posicion[0]=i;
-													m[i][j].tipo_pieza.posicion[1]=j;
+													matriz_setPieza(m,i,j,'b',true,'Q',i,j);
 												}else{
 													if(i==7 && j==3){
-														m[i][j].color='w';
-														m[i][j].primer_turno=true;
-														m[i][j].tipo_pieza.nombre='K';
-														m[i][j].tipo_pieza.posicion[0]=i;
-														m[i][j].tipo_pieza.posicion[1]=j;
+														matriz_setPieza(m,i,j,'w',true,'K',i,j);
 													}else{
 														if(i==7 && j==4){
-															m[i][j].color='w';
-															m[i][j].primer_turno=true;
-															m[i][j].tipo_pieza.nombre='Q';
-															m[i][j].tipo_pieza.posicion[0]=i;
-															m[i][j].tipo_pieza.posicion[1]=j;
+															matriz_setPieza(m,i,j,'w',true,'Q',i,j);
 														}else{
-															m[i][j].color='V';
-															m[i][j].primer_turno=true;
-															m[i][j].tipo_pieza.nombre='V';
-															m[i][j].tipo_pieza.posicion[0]=i;
-															m[i][j].tipo_pieza.posicion[1]=j;
+															matriz_setPieza(m,i,j,'V',true,'V',i,j);
 														}						
 													}
 												}
@@ -112,6 +60,15 @@ void tablero_inicio(p (*m)[8]){
 			}
 		}
 	}
+}
+
+//Funcion para cambiar el valor de una casilla de la matriz
+void matriz_setPieza( p (*m)[8],int i, int j,char color, bool turno,char nombre,int posicion_1,int posicion_2){
+	m[i][j].color=color;
+	m[i][j].primer_turno=turno;
+	m[i][j].tipo_pieza.nombre=nombre;
+	m[i][j].tipo_pieza.posicion[0]=posicion_1;
+	m[i][j].tipo_pieza.posicion[1]=posicion_2;
 }
 
 void mostrar_tablero(p (*m)[8]){
