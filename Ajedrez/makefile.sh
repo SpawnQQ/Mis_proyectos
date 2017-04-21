9e96b2aa37e0a02e72ddda20a5f61413bc30d564
SpawnQQ <nick.aguirre.16@gmail.com>
2017-04-21 14:43:57 -0300

    Se modifico el makefile.sh, saluda al usuario logeado con "whoami", ademas se agrego las caracteristicas del sistema operativo y el nombre del mismo

--- Ajedrez/makefile.sh
+++ Ajedrez/makefile.sh
@@ -1,5 +1,8 @@
 echo -e "Compilando..."
 gcc ajedrez.c -std=c99 -o ajedrez -I $(pwd ajedrez.c)
-echo -e "Ejecutando desde $(pwd ajedrez.c)"
+clear
+echo -e "BIENVENIDO $(whoami)!!!"
+echo -e "Ubicacion del programa $(pwd ajedrez.c)"
+echo -e "En un sistema $(cat /etc/issue), $(uname -m)"
 ./ajedrez
 echo -e "Programa finalizado"
