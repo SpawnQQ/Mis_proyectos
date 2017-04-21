15604c03897bc23a8727f3b27c25c916cb241466
SpawnQQ <nick.aguirre.16@gmail.com>
2017-04-21 17:47:14 -0300

    Se agregaron instalaciones y actualizacion al sistema

--- Ajedrez/instalador.sh
+++ Ajedrez/instalador.sh
@@ -1,2 +1,6 @@
+echo -e "Primero actualizamos el sistema"
+sudo aptitude update && aptitude upgrade
 echo -e "Se instala mpg123 para reproducir sonido"
 sudo aptitude install mpg123
+echo -e "Se instala la libreria curses.h, para getch()"
+sudo apt-get install libncurses5-dev libncursesw5-dev
