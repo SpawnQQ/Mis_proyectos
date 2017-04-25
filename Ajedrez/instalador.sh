9b8601c82a0fd5453699f65a2f266e6ab568cc7d
SpawnQQ <nick.aguirre.16@gmail.com>
2017-04-24 21:59:31 -0300

    Se cambio la libreria SDL a SDL2, porque habia problemas con las fucniones.

--- Ajedrez/instalador.sh
+++ Ajedrez/instalador.sh
@@ -2,6 +2,8 @@ echo -e "Primero actualizamos el sistema"
 sudo aptitude update && aptitude upgrade
 echo -e "Se instala mpg123 para reproducir sonido"
 sudo aptitude install mpg123
-echo -e "Se instalara paquetes para graficos SDL2"
-sudo aptitude install libsdl2-2.0-0
-sudo apt-get install libsdl2-dev
+echo -e "Se instalara paquetes para graficos SDL"
+sudo apt-get install libsdl1.2-dev
+sudo apt-get install libsdl-image1.2-dev 
+sudo apt-get install libsdl-mixer1.2-dev 
+sudo apt-get install libsdl-ttf2.0-dev
