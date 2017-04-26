89bc4c57d991f25a5dd4fab010e01b543628d6fb
SpawnQQ <nick.aguirre.16@gmail.com>
2017-04-26 15:19:36 -0300

    Se encontro la forma de agregar png, ahora se ensamblara el codigo SDL para mostrar por pantalla el tablero.

--- SDL_Probando/archivo.c
+++ SDL_Probando/archivo.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
 
-#define WIDTH 720	
-#define HEIGHT 650
+#define WIDTH 460
+#define HEIGHT 380
 #define BPP 24
+#define MASK 255,255,255
 
-SDL_Surface *tablero, *screen;
+SDL_Surface *tablero, *screen, *peon;
 SDL_Rect dest;
 SDL_Event event;
 int done = 0;
@@ -32,12 +34,14 @@ int main(){
     	exit(1);
 	}
 
-	tablero = SDL_LoadBMP("imagenes/tablero.bmp");
+	tablero = SDL_LoadBMP("imagenes/tablero_cafe2.bmp");
 	if(tablero == NULL)
 	{
     	printf("No se ha podido cargar la imagen: %s\n", SDL_GetError());
     	exit(1);
 	}
+
+
 
 	minave.x = 50;
 	minave.y = 10;
@@ -50,6 +54,31 @@ int main(){
 	SDL_BlitSurface(tablero, NULL, screen, &dest);
 	SDL_Flip(screen);
 	SDL_FreeSurface(tablero);
+
+	//peon = SDL_LoadBMP("imagenes/Reina4.bmp");
+	peon=IMG_Load("imagenes/piezas/Rw.png");
+	if(peon == NULL)
+	{
+    	printf("No se ha podido cargar la imagen: %s\n", SDL_GetError());
+    	exit(1);
+	}
+
+	dest.x = 50+(7*44);
+	dest.y = 8+(0*44);
+
+	// (x,y)=50,8 8a
+	// (x,y)=50,52 7a
+	// (x,y)=50,96 6a
+	// (x,y)=
+	// f(x,y)=x,44*n, si el primer n es 8  Para la columna, para la fila deberia ser similar
+
+	dest.w = peon -> w;
+	dest.h = peon -> h;
+
+	SDL_BlitSurface(peon, NULL, screen, &dest);
+	SDL_Flip (screen);
+	SDL_FreeSurface(peon);
+
 
 	keys = SDL_GetKeyState(NULL);
 	 // Esperamos la pulsación de una tecla para salir 
--- SDL_Probando/makefile.sh
+++ SDL_Probando/makefile.sh
@@ -1,2 +1,2 @@
-gcc -g -Wall -lSDL $(pkg-config --cflags --libs sdl) -o programa archivo.c -I $(pwd archivo.c)
+gcc -g -Wall -lSDL -lSDL_image $(pkg-config --cflags --libs sdl) -o programa archivo.c -I $(pwd archivo.c)
 ./programa
