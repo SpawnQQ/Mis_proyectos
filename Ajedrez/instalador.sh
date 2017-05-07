5560a2fbf15079b2911b3de47c0f72527741bd4a
nicolasaguirre <nick.aguirre.16@gmail.com>
2017-05-07 03:51:23 -0300

    Se modifico el instalador.

--- Ajedrez/instalador.sh
+++ Ajedrez/instalador.sh
@@ -1,3 +1,4 @@
+sudo apt-get install aptitude
 echo -e "Primero actualizamos el sistema"
 sudo aptitude update && aptitude upgrade
 echo -e "Se instala mpg123 para reproducir sonido"
@@ -9,5 +10,5 @@ sudo apt-get install libsdl-image1.2-dev
 sudo apt-get install libsdl-mixer1.2-dev
 sudo apt-get install libsdl-ttf2.0-0 
 sudo apt-get install libsdl-ttf2.0-dev
-sudo aptitude install libsdl-gfx1.2-dev libsdl-gfx1.2-5
 sudo apt-get install libsdl-net1.2 libsdl-net1.2-dev
+sudo apt-get install libsdl-gfx1.2-5 libsdl-gfx1.2-dev libsdl-gfx1.2-doc
